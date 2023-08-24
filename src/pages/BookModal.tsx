39d57061e75853848b8a89d86b57601e308a9302
Rizal Nasrulloh <rizalxrpl2@gmail.com>
2023-08-24 09:36:35 +0800

    feat: book modal

---
+++ src/pages/BookModal.tsx
@@ -0,0 +1,133 @@
+import { Button, Card, Col, Collapse, DatePicker, Descriptions, Divider, Image, Input, Modal, Row, Segmented, Space, TimePicker } from "antd"
+import React, { useState } from "react"
+import { SearchOutlined, DownOutlined, MinusOutlined, PlusOutlined,
+  PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
+
+const { RangePicker } = TimePicker
+const { TextArea } = Input
+const detailItems = [
+  {
+    key: 1,
+    label: 'Details amount',
+    children: 
+    <div>
+      <Row justify="space-between">
+        <Col>Subtotal</Col>
+        <Col>$100.00</Col>
+      </Row>
+      <Row justify="space-between">
+        <Col>Discount (50%)</Col>
+        <Col>-$50.00</Col>
+      </Row>
+      <Row justify="space-between">
+        <Col>Credit (1h)</Col>
+        <Col>-$5.00</Col>
+      </Row>
+    </div>
+  }
+]
+const pricingItems = [
+  {key: 1, label: 'All day', children: <span><b>$40</b>/day</span>},
+  {key: 2, label: 'Half day', children: <span><b>$20</b>/day</span>},
+  {key: 3, label: 'Hourly', children: <span><b>$10</b>/hour</span>}
+]
+
+const BookModal: React.FC = (props) => {
+  const [payment, setPayment] = useState('hourly')
+  const [quantity, setQuantity] = useState(1)
+  const [bookingNotes, setBookingNotes] = useState(false)
+  const paymentList = [
+    { label: 'Hourly', value: 'hourly' },
+    { label: 'All Day', value: 'all_day' },
+    { label: 'Half Day', value: 'half_day' }
+  ]
+  return (
+    <Modal
+      title="Event space"
+      width={1000}
+      centered
+      open={props.modalBook}
+      onCancel={() => props.setModalBook(false)}
+      footer={null}
+    >
+      <p>Pyrmont, Sydney</p>
+      <Row gutter={10}>
+        <Col xl={12}>
+          <Image src="images/img_modal1.png" />
+          <Space style={{marginTop: '5px'}}>
+            <Image src="images/img_modal1.png" />
+            <Image src="images/img_modal1.png" />
+            <Image src="images/img_modal1.png" />
+            <Image src="images/img_modal1.png" />
+            <Image src="images/img_modal1.png" />
+          </Space>
+          <h3>Description</h3>
+          <p>One of our standout spaces is our spacious and well-lit room, designed to cater to teams or individuals who require a private and productive workspace. This room is equipped with comfortable chairs, adjustable desks, high-speed internet, and plenty of power...</p>
+          <Button type="link" style={{padding: 0}}>Show more</Button>
+          <Divider dashed style={{margin: '5px 0'}}/>
+          <h3>Pricing</h3>
+          <Descriptions items={pricingItems} />
+          <Divider dashed style={{margin: '5px 0'}}/>
+        </Col>
+        <Col xl={12}>
+          <Card size='small' bodyStyle={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', background: '#F3F4FA', minHeight: '550px'}} headStyle={{border: 'none', background: '#F3F4FA'}} title="Booking Details">
+            <Segmented value={payment} onChange={(v) => setPayment(v)} options={paymentList} />
+            <Input style={{marginTop: '10px'}} placeholder="North Strathfield" prefix={<SearchOutlined />} suffix={<DownOutlined />} />
+            <Row style={{marginTop: '10px'}} justify="space-between">
+              <Col>
+                <DatePicker />
+              </Col>
+              <Col>
+                <RangePicker disabled={payment !== 'hourly'} use12Hours format="h:mm a" />
+              </Col>
+            </Row>
+            <Row style={{marginTop: '10px'}} justify="space-between">
+              <Col>
+                Quantity
+              </Col>
+              <Col>
+                <Button size="small" onClick={() => setQuantity(quantity - 1)}><MinusOutlined /></Button>
+                <span style={{margin: '0 10px'}}>{quantity}</span>
+                <Button size="small" onClick={() => setQuantity(quantity + 1)}><PlusOutlined /></Button>
+              </Col>
+            </Row>
+            <Divider dashed style={{margin: '5px 0'}}/>
+            {bookingNotes ?
+              <div>
+                <Button onClick={() => setBookingNotes(!bookingNotes)} type="link" size="small">
+                  <PlusCircleOutlined /> Booking Notes
+                </Button>
+              </div>
+              :
+              <div style={{ marginBottom: '15px' }}>
+                <Button onClick={() => setBookingNotes(!bookingNotes)} style={{color: 'red'}} type="link" size="small">
+                  <MinusCircleOutlined /> Remove Booking Notes
+                </Button>
+                <TextArea rows={2} placeholder="Add note here..." />
+              </div>
+            }
+
+            <Card style={{marginTop: 'auto'}} size="small">
+              <Collapse expandIconPosition="end" size="small" ghost items={detailItems} />
+              <Divider style={{margin: '5px 0'}}/>
+              <div style={{padding: '9px'}}>
+                <Row justify="space-between">
+                  <Col>GST</Col>
+                  <Col>$4.55</Col>
+                </Row>
+                <Row justify="space-between">
+                  <Col><span>Total</span></Col>
+                  <Col><span>$44.55</span></Col>
+                </Row>
+              </div>
+            </Card>
+
+            <Button onClick={() => props.setModalBook(false)} type="primary" style={{marginTop: '10px'}}>Add to Cart</Button>
+          </Card>
+        </Col>
+      </Row>
+    </Modal>
+  )
+}
+
+export default BookModal;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,186 @@
+import React, { useState } from 'react';
+import { Button, Card, Col, DatePicker, Divider, Image, Input, Layout, Menu, Radio, Row, Segmented, Select, Space, Tag, TimePicker } from 'antd';
+import type { MenuProps } from 'antd';
+import { HeartOutlined, BellOutlined, ShoppingCartOutlined, 
+  CalendarOutlined, ApartmentOutlined, UnorderedListOutlined, 
+  AppstoreOutlined, CreditCardOutlined, SearchOutlined, DownOutlined,
+  PhoneFilled, MailFilled, CompassOutlined, ClockCircleOutlined, UserOutlined } from '@ant-design/icons';
+import Search from 'antd/es/input/Search';
+import Sider from 'antd/es/layout/Sider';
+import BookModal from './BookModal.tsx';
+
+const { Option } = Select;
+const { Header, Content } = Layout;
+const { RangePicker } = TimePicker
+
+const items: MenuProps['items'] = [
+  { key: 1, label: <HeartOutlined /> },
+  { key: 2, label: <BellOutlined /> },
+  { key: 3, label: <Button type="default"><ShoppingCartOutlined /> Cart</Button>  },
+  { key: 4, label: <Button type="default"><ShoppingCartOutlined /> Sign In</Button> }
+];
+
+const Home: React.FC = () => {
+  const [sort, setSort] = useState('unordered')
+  const [formatHour, setFormatHour] = useState('am')
+  const [payment, setPayment] = useState('hourly')
+  const [modalBook, setModalBook] = useState(false)
+  const paymentList = [
+    { label: 'Hourly', value: 'hourly' },
+    { label: 'All Day', value: 'all_day' },
+    { label: 'Half Day', value: 'half_day' }
+  ]
+  return (
+    <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          background: 'white'
+        }}
+      >
+        <Image src="Logo.png" alt="logo hamlet" />
+        <div className="demo-logo" />
+        <Menu
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          items={items}
+          style={{ marginLeft: 'auto' }}
+        />
+      </Header>
+      <div style={{ display: 'flex', padding: '10px 50px', background: 'white', borderTop: '1px solid #0306201A' }}>
+        <Space>
+          <Button shape="round" type="default">
+            <CalendarOutlined /> Meeting Room
+          </Button>
+          <Button shape="round" type="default">
+            <ApartmentOutlined /> Desk Pass
+          </Button>
+          <Button shape="round" type="default">
+            More
+          </Button>
+        </Space>
+        <Space style={{ marginLeft: 'auto' }}>
+          <Search placeholder="Search room or member..." allowClear style={{ width: 200 }} />
+
+          <Select defaultValue="all">
+            <Option value="all">Filter By: All</Option>
+            <Option value="name">Filter By: Name</Option>
+          </Select>
+
+          <Radio.Group value={sort} onChange={(e) => setSort(e.target.value)} buttonStyle="solid">
+            <Radio.Button value="unordered"><UnorderedListOutlined /></Radio.Button>
+            <Radio.Button value="ordered"><AppstoreOutlined /></Radio.Button>
+          </Radio.Group>
+        </Space>
+      </div>
+      <Layout hasSider style={{ padding: '10px 50px' }}>
+        <Content>
+          <Card size="small">
+            <div style={{display: 'flex'}}>
+              <Segmented value={payment} onChange={(v) => setPayment(v)} options={paymentList} />
+
+              {payment === "half_day" &&
+                <Radio.Group style={{marginLeft: '20px'}} value={formatHour} onChange={(e) => setFormatHour(e.target.value)}>
+                  <Radio.Button value="am">AM</Radio.Button>
+                  <Radio.Button value="pm">PM</Radio.Button>
+                </Radio.Group>
+              }
+
+              <Tag style={{ marginLeft: 'auto', alignSelf: 'center' }}>
+                <CreditCardOutlined style={{ marginRight: '5px', color: '#1677FF' }} />
+                <b>2 hrs</b>
+              </Tag>
+            </div>
+
+            <Row style={{marginTop: '10px'}} gutter={10} justify="space-between">
+              <Col span={14}>
+                <Input placeholder="North Strathfield" prefix={<SearchOutlined />} suffix={<DownOutlined />} />
+              </Col>
+              <Col>
+                <DatePicker />
+              </Col>
+              <Col>
+                <RangePicker disabled={payment !== 'hourly'} use12Hours format="h:mm a" />
+              </Col>
+            </Row>
+          </Card>
+          <div style={{ margin: '16px 0' }}>
+            Found 12 spaces
+          </div>
+          <div>
+            <Row gutter={[12, 12]} >
+              <Col md={12} sm={24} xs={24}>
+                <Card size="small">
+                  <Row gutter={15}>
+                    <Col>
+                      <Image src="images/img_place2.png" alt="" />
+                    </Col>
+                    <Col span={16}>
+                      <Row justify="space-between">
+                        <Col>
+                          <b>Room 01</b>
+                        </Col>
+                        <Col>
+                          <Tag color="green">Available</Tag>
+                        </Col>
+                      </Row>
+                      <p>
+                        <ClockCircleOutlined /> All day (9:00am - 6:00pm)<br/>
+                        <SearchOutlined /> Pyrmont, Sydney
+                      </p>
+                      <Row justify="space-between">
+                        <Col>
+                          <span style={{fontSize: '18px'}}>$12</span> <span>per hr</span>
+                          <Divider type="vertical" />
+                          <UserOutlined /> <b>20</b>
+                        </Col>
+                        <Col>
+                          <Button onClick={() => setModalBook(true)} type="primary" size="middle">Book</Button>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Card>
+                <BookModal modalBook={modalBook} setModalBook={(e: boolean) => setModalBook(e)} />
+              </Col>
+            </Row>
+          </div>
+        </Content>
+
+        <Sider width={500} breakpoint="md" collapsedWidth="0" style={{ background: 'transparent', padding: '0 0 0 20px' }}>
+          <Card>
+            <Row gutter={10}>
+              <Col span={14}>
+                <div>
+                  <b>Bondi Junction, Sydney NSW</b><br/>
+                  <span style={{color: '#9A9A9A'}}>33 Bondi Road, Bondi Junction NSW 2000</span>
+                </div>
+                <p>
+                  <b>Open hours</b><br/>
+                  <span>Mon to Fri 9am - 6pm</span><br/>
+                  <span>After hours bookings <span style={{color: '#2F54EB'}}>Request here</span></span>
+                </p>
+                <p>
+                  <PhoneFilled /> <span style={{color: '#2F54EB'}}>+61 02 924 577</span>
+                </p>
+                <p>
+                  <MailFilled /> <span style={{color: '#2F54EB'}}>reception@space.com</span>
+                </p>
+                <p>
+                  <CompassOutlined /> <span style={{color: '#2F54EB'}}>www.space.com</span>
+                </p>
+              </Col>
+              <Col>
+                <Image src='images/img_place1.png' alt='place1' />
+              </Col>
+              <Image src="images/img_map.png" alt="map" style={{width: '100%'}} />
+            </Row>
+          </Card>
+        </Sider>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Home;
